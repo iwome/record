e6a6b79b112eeb44d75dcc841e0041ec928e7f79
bangbang.qiu <431066566@qq.com>
2019-05-05 18:02:33 +0800

    dart 语法

--- Android/flutter_learn/lib/dartLearn.dart
+++ Android/flutter_learn/lib/dartLearn.dart
@@ -29,6 +29,13 @@ class Rectangle {
   //成员对象默认值必须是编译器就确认的常量
   Rectangle({this.origin = const Point(0, 0), this.width = 0, this.height = 0});
 
+  //可选命名参数 {}表示 传参时必须使用参数名：value的形式
+  void nameMethod({String a, int b}) {}
+
+  //可选位置参数 []表示
+  //命名和位置参数区别，前者可放在前面，后者只能放在最后，所以前者要命名后者无需
+  void positionMethod(String a, int b, [int c, int d]) {}
+
   @override
   String toString() =>
       'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
@@ -41,6 +48,7 @@ abstract class Shape {
   factory Shape(String type) {
     if (type == 'circle') return Circle(2);
     if (type == 'square') return Square(2);
+    if (type == 'circleMock') return CircleMock();
     throw 'Can\'t create $type.';
   }
 }
@@ -69,20 +77,34 @@ class CircleMock implements Circle {
 
   num radius;
 
+  int color;
+
+  void methodA() {}
+
+  void methodB() {}
+
   @override
   void drawCircle() {
     // TODO: implement _drawCircle
   }
-
 }
 
 //主函数，函数、变量等可声明在类外
 void main() {
   //关键字 new 可省略
   var bike = Bicycle(2, 0, 1);
-  print(Rectangle(origin: const Point(10, 10)));
+  var rectangle = Rectangle(origin: const Point(10, 10));
+  print(rectangle);
+  rectangle.nameMethod(a: "name");
+  rectangle.positionMethod("a", 9, 10);
   print(bike);
-
+  CircleMock cm = Shape('circleMock');
+  //..表示 级联函数
+  cm
+    ..methodA()
+    ..methodB()
+    ..area = 9
+    ..color = 6;
   print(Shape('circle').area);
   print(Shape('square').area);
 }
